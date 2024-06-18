606965af143d03a3c94386316ba348ddc0265cc6
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-18 19:45:30 -0300

    exercicio 2 adicionado

--- src/main/java/com/introducao/lista/Lista2.java
+++ src/main/java/com/introducao/lista/Lista2.java
@@ -31,6 +31,32 @@ public class Lista2 {
 
     public void ex02(){
 
+        Scanner scanner = new Scanner(System.in);
+
+        int numero1;
+        int numero2;
+
+        System.out.println("Escreva um numero: ");
+        numero1 = scanner.nextInt();
+
+        System.out.println("Escreva outro numero para comparar: ");
+        numero2 = scanner.nextInt();
+
+        if(numero1 > numero2) {
+
+            System.out.println("O numero " + " " + numero1 + " " + "é maior!");
+
+        } else if(numero2 > numero1){
+
+            System.out.println("O numero " + " " + numero2 + " " + "é maior!");   
+
+        } else {
+         
+            System.out.println("os numeros são iguais!");
+
+        }
+
+        scanner.close();
 
     }
 }
--- src/main/java/com/introducao/lista/ListaApplication.java
+++ src/main/java/com/introducao/lista/ListaApplication.java
@@ -6,20 +6,20 @@ import org.w3c.dom.ls.LSException;
 @SpringBootApplication
 public class ListaApplication {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		SpringApplication.run(ListaApplication.class, args);
 
 		Lista1 lista = new Lista1();
 		lista.ex01();
 		
-	}
+	} */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListaApplication.class, args);
 
 		Lista2 lista02 = new Lista2();
-		lista02.ex01();
+		lista02.ex02();
 		
-	}
+	} 
 
 }
