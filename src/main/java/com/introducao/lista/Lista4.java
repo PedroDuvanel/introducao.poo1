4bda46e374a181a1d2320ffdbfbe468877038781
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-24 15:28:11 -0300

    Adicionado o exercicio 5

--- src/main/java/com/introducao/lista/Lista4.java
+++ src/main/java/com/introducao/lista/Lista4.java
@@ -1,10 +1,11 @@
 package com.introducao.lista;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lista4 {
 
-    public void ex01(){
+    public void ex01() {
 
         int[] vendas = { 10, 13, 9, 80, 140, 30, 20, 25, 90, 80, 110, 150 };
         int valor = 0;
@@ -21,7 +22,7 @@ public class Lista4 {
 
     }
 
-    public void ex02(){
+    public void ex02() {
 
         int[] vendas = { 10, 13, 9, 80, 140, 30, 20, 25, 90, 80, 110, 150 };
         int menorValor = vendas[0];
@@ -30,9 +31,9 @@ public class Lista4 {
         System.out.println("--------Bem vindo ao supermarket--------");
         System.out.println("----------------------------------------");
 
-        for(int i = 0; i < vendas.length; i++){
+        for (int i = 0; i < vendas.length; i++) {
 
-            if(vendas[i] < menorValor){
+            if (vendas[i] < menorValor) {
 
                 menorValor = vendas[i];
 
@@ -40,9 +41,9 @@ public class Lista4 {
 
         }
 
-        for (int i = 0; i < vendas.length; i++){
+        for (int i = 0; i < vendas.length; i++) {
 
-            if(vendas[i] > maiorValor){
+            if (vendas[i] > maiorValor) {
 
                 maiorValor = vendas[i];
 
@@ -54,9 +55,9 @@ public class Lista4 {
 
         System.out.println("O maior valor é: " + maiorValor + " R$ ");
 
-    } 
+    }
 
-    public void ex03(){
+    public void ex03() {
 
         int[] vendas = { 10, 13, 9, 80, 140, 30, 20, 25, 90, 80, 110, 150 };
         int valor = 0;
@@ -66,7 +67,7 @@ public class Lista4 {
 
         int media;
 
-        for (int i = 0; i < vendas.length; i ++){
+        for (int i = 0; i < vendas.length; i++) {
 
             valor += vendas[i];
 
@@ -78,14 +79,14 @@ public class Lista4 {
 
     }
 
-    public void ex04(){
+    public void ex04() {
 
-        int [] lista = { 3, 5, 6, 7, 8, 10, 22, 55, 110 };
+        int[] lista = { 3, 5, 6, 7, 8, 10, 22, 55, 110 };
         int par = 0;
 
-        for (int i = 0 ; i < lista.length ; i++){  
+        for (int i : lista) {
 
-            if( i % 2 == 0){
+            if (i % 2 == 0) {
 
                 par++;
 
@@ -97,11 +98,51 @@ public class Lista4 {
 
     }
 
-    public void ex05(){
+    public void ex05() {
 
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> lista = new ArrayList<>();
+        int opcao;
 
+        System.out.println("------Bem vindos ao supermarket!-------");
+
+        do {
+            System.out.println("-------------MENU-------------");
+            System.out.println("Digite a opção desejada: ");
+            System.out.println("1- Adicionar item à lista.");
+            System.out.println("2- Exibir lista.");
+            System.out.println("3- Sair.");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o item a ser adicionado à lista:");
+                    String item = scanner.nextLine();
+                    lista.add(item);
+                    System.out.println("Item adicionado à lista.");
+                    break;
+                case 2:
+                    System.out.println("------- Lista de Compras -------");
+                    for (String listItem : lista) {
+                        System.out.println("- " + listItem);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saindo do Supermarket. Até logo!");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Digite novamente.");
+                    break;
+            }
+        } while (opcao != 3);
+
+        scanner.close();
+    }
+
+    public void ex06() {
+
+        
 
     }
 }
-
-
